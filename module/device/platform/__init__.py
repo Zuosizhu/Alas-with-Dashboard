382dfb5657e83577b20f7f2c0964367cc4bc4a09
Zuosizhu <2839299264@qq.com>
2024-05-27 08:47:15 +0800

    公告

--- module/device/platform/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-if sys.platform == 'win32':
-    from module.device.platform.platform_windows import PlatformWindows as Platform
-else:
-    from module.device.platform.platform_base import PlatformBase as Platform
