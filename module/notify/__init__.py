672ba3a5b4fa9135ecc387cc77f388cc587076e1
Coldaine <Coldaine@users.noreply.github.com>
2026-01-16 13:47:06 -0600

    feat: restructure project into monorepo with vendor branch pattern

--- module/notify/__init__.py
+++
@@ -1,4 +0,0 @@
-def handle_notify(*args, **kwargs):
-    # Lazy import onepush
-    from module.notify.notify import handle_notify
-    return handle_notify(*args, **kwargs)
--- run_gui.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting ALAS GUI...
-python-3.7.9-embed-amd64\python.exe gui.py
-pause
